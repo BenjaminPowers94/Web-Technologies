a41379ac3388d55d74f6f4b97f8d29b694ba72db
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 16:04:16 -0500

    Add files via upload

---
+++ Assignment 2 source code/HMBPMembershipType.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * HMBPMembershipType.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HMBPSail.Models;
+
+namespace HMBPSail.Controllers
+{
+    public class HMBPMembershipType : Controller
+    {
+        private readonly SailContext _context;
+
+        public HMBPMembershipType(SailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HMBPMembershipType
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MembershipType.ToListAsync());
+        }
+
+        // GET: HMBPMembershipType/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipType
+                .FirstOrDefaultAsync(m => m.MembershipTypeName == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        // GET: HMBPMembershipType/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HMBPMembershipType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MembershipTypeName,Description,RatioToFull")] MembershipType membershipType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(membershipType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+
+        // GET: HMBPMembershipType/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipType.FindAsync(id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+            return View(membershipType);
+        }
+
+        // POST: HMBPMembershipType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MembershipTypeName,Description,RatioToFull")] MembershipType membershipType)
+        {
+            if (id != membershipType.MembershipTypeName)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(membershipType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembershipTypeExists(membershipType.MembershipTypeName))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+
+        // GET: HMBPMembershipType/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipType
+                .FirstOrDefaultAsync(m => m.MembershipTypeName == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        // POST: HMBPMembershipType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var membershipType = await _context.MembershipType.FindAsync(id);
+            _context.MembershipType.Remove(membershipType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembershipTypeExists(string id)
+        {
+            return _context.MembershipType.Any(e => e.MembershipTypeName == id);
+        }
+    }
+}
---
+++ Assignment 2 source code/HMBPMembershipsController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HMBPSail.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace HMBPSail.Controllers
+{
+    public class HMBPMembershipsController : Controller
+    {
+        private readonly SailContext _context;
+
+        public HMBPMembershipsController(SailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HMBPMemberships
+        public async Task<IActionResult> Index()
+        {
+            var memberId = Request.Query["memberId"].FirstOrDefault();
+            var sailContext = _context.Membership.Include(m => m.Member)
+                            .Include(m => m.MembershipTypeNameNavigation)
+                            .Where(m => m.MemberId == Convert.ToInt32(memberId)) // conditions to find the display the memberId
+                            .OrderByDescending(m => m.Year); // Order most recent years.
+           
+            if (memberId != null) // if the memberId is not in the queryString. Save it to session variable.
+            {
+                HttpContext.Session.SetInt32("memberId", Convert.ToInt32(memberId.ToString()));
+            }
+            else // return to the controller and display tempdata if memberId neither in queryString nor session varible.
+            {
+                TempData["message"] = "Please select a member";
+                return RedirectToAction("Index", "HMBPMembers");
+            }
+            return View(await sailContext.ToListAsync());
+        }
+
+        // GET: HMBPMemberships/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membership = await _context.Membership
+                .Include(m => m.Member)
+                .Include(m => m.MembershipTypeNameNavigation)
+                .FirstOrDefaultAsync(m => m.MembershipId == id);
+            if (membership == null)
+            {
+                return NotFound();
+            }
+
+            return View(membership);
+        }
+
+        // GET: HMBPMemberships/Create
+        public IActionResult Create()
+        {
+            ViewData["MemberId"] = new SelectList(_context.Member, "MemberId", "FirstName");
+            ViewData["MembershipTypeName"] = new SelectList(_context.MembershipType.OrderBy(m => m.MembershipTypeName), "MembershipTypeName", "MembershipTypeName"); // order by year 
+            return View(); 
+        }
+
+        // POST: HMBPMemberships/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MembershipId,MemberId,Year,MembershipTypeName,Fee,Comments,Paid")] Membership membership)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(membership);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "MemberId", "FirstName", membership.MemberId);
+            ViewData["MembershipTypeName"] = new SelectList(_context.MembershipType, "MembershipTypeName", "MembershipTypeName", membership.MembershipTypeName);
+            return View(membership);
+        }
+
+        // GET: HMBPMemberships/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membership = await _context.Membership.FindAsync(id);
+            if (membership == null)
+            {
+                return NotFound();
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "MemberId", "FirstName", membership.MemberId);
+            ViewData["MembershipTypeName"] = new SelectList(_context.MembershipType, "MembershipTypeName", "MembershipTypeName", membership.MembershipTypeName);
+            return View(membership);
+        }
+
+        // POST: HMBPMemberships/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MembershipId,MemberId,Year,MembershipTypeName,Fee,Comments,Paid")] Membership membership)
+        {
+            if (id != membership.MembershipId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Display error when user edit the pior year.
+                    if(membership.Year != DateTime.Now.Year)
+                    {
+                        TempData["message"] = "Cannot edit the information of the pior year";
+                        return RedirectToAction(nameof(Edit));
+                    }
+                    else
+                    {
+                        _context.Update(membership);
+                        await _context.SaveChangesAsync();
+                    }                  
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembershipExists(membership.MembershipId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "MemberId", "FirstName", membership.MemberId);
+            ViewData["MembershipTypeName"] = new SelectList(_context.MembershipType.OrderBy(m => m.MembershipTypeName), "MembershipTypeName", "MembershipTypeName", membership.MembershipTypeName);
+            return View(membership);
+        }
+
+        // GET: HMBPMemberships/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membership = await _context.Membership
+                .Include(m => m.Member)
+                .Include(m => m.MembershipTypeNameNavigation)
+                .FirstOrDefaultAsync(m => m.MembershipId == id);
+            if (membership == null)
+            {
+                return NotFound();
+            }
+
+            return View(membership);
+        }
+
+        // POST: HMBPMemberships/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var membership = await _context.Membership.FindAsync(id);
+            _context.Membership.Remove(membership);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembershipExists(int id)
+        {
+            return _context.Membership.Any(e => e.MembershipId == id);
+        }
+    }
+}
